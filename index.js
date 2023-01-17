1f0c7162869a470760cd35838ac83f0eace4d4d5
Sergey Svekla <sergey.svekla@gmail.com>
2023-01-17 14:10:31 +0200

    one commit

---
+++ index.js
@@ -0,0 +1,7 @@
+sayHello();
+function sayHello() {
+  const userName = prompt("What is your name?");
+  const userWellcome = "Hello, ";
+  const userQuestion = "! How are you?";
+  alert(`${userWellcome} ${userName} ${userQuestion}`);
+}
